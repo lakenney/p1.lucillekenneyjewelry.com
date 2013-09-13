fe01cec2e345bdd8dac4f47ad4effe662954ca3a
Lucille Kenney <lucilleakenney@gmail.com>
2013-09-13 19:18:48 -0400

    Fixed a typo and testing that umask 022 change in bashrc file

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
             Hi and welcome! I am a Graphic and Web Designer/Developer based out of Boston. My specialty is hand coding HTML and CSS with a focus on SEO and accessibility. I've also worked in IT in the past trouble shooting technical graphic problems for designers, engineers and architects using the Adobe Applications.
         </p>
         <p>
-            My strength is in my ability to apply a unique balance of artistic and technical skills to web technologies and print in terms of design constraints and potential. And I'm always seeking out the latest, best-pact ices to stay abreast of the market.
+            My strength is in my ability to apply a unique balance of artistic and technical skills to web technologies and print in terms of design constraints and potential. And I'm always seeking out the latest, best-pactices to stay abreast of the market.
         </p>
         <p>
             Oh, and I love working with metal clay and Swarovski crystals. Metal clay is fine silver (also comes in other metals) mixed with a gluten to give it a clay consistency. The gluten is burned off in the kiln process and the final piece is fine silver. So as you've probably guess, by my domain name, I'll be creating a site where I'll showcase and sell my jewelry. 
