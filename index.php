598d0ae4a80ca30040759af7af614726c45e7a7b
Lucille Kenney <lucilleakenney@gmail.com>
2013-09-16 22:33:29 -0400

    Added favicon and cleaned up code

--- index.php
+++ index.php
@@ -1,32 +1,33 @@
 <!DOCTYPE html>
 <html>
-<head>
+    <head>
 
-<meta charset="utf-8">
-<title>Project: P1</title>
-<meta name="description" content="CSCI E-15 Dynamic Web Applications with Susan Buck. Project: P1">
-<meta name="author" content="Lucille Kenney Hybridge web designer developer">
+        <meta charset="utf-8">
+        <title>Project: P1</title>
+        <meta name="description" content="CSCI E-15 Dynamic Web Applications with Susan Buck. Project: P1">
+        <meta name="author" content="Lucille Kenney Hybridge web designer developer">
 
-<link rel="stylesheet" href="/css/styles.css" />
+        <link href="favicon.ico" rel="icon" type="image/x-icon" />
+        <link rel="stylesheet" href="/css/styles.css" />
 
-</head>
-<body>
-<div id="wrapper">
-    <h1>Lucille Kenney</h1>
-    <h2>My small bio</h2>
-        <p>
-            Hi and welcome! I am a Graphic and Web Designer/Developer based out of Boston. My specialty is hand coding HTML and CSS with a focus on SEO and accessibility. I've also worked in IT in the past trouble shooting technical graphic problems for designers, engineers and architects using the Adobe Applications.
-        </p>
-        <p>
-            My strength is in my ability to apply a unique balance of artistic and technical skills to web technologies and print in terms of design constraints and potential. And I'm always seeking out the latest, best-pactices to stay abreast of the market.
-        </p>
-        <p>
-            Oh, and I love working with metal clay and Swarovski crystals. Metal clay is fine silver (also comes in other metals) mixed with a gluten to give it a clay consistency. The gluten is burned off in the kiln process and the final piece is fine silver. So as you've probably guess, by my domain name, I'll be creating a site where I'll showcase and sell my jewelry. 
-        </p>
-    <h2>Name of code editor I use</h2>
-        <p>
-            I'm transitioning from Dreamweaver to Sublime 2. I believe that all of this great local and server setup information will expedite that transition.
-        </p>
-</div><!--end wrapper-->
-</body>
+    </head>
+    <body>
+        <div id="wrapper">
+            <h1>Lucille Kenney</h1>
+            <h2>My small bio</h2>
+                <p>
+                    Hi and welcome! I am a Graphic and Web Designer/Developer based out of Boston. My specialty is hand coding HTML and CSS with a focus on responsive design and an eye toward SEO and accessibility. I've also worked in IT in the past trouble shooting print and technical graphic problems for designers, engineers and architects using the Adobe Applications.
+                </p>
+                <p>
+                    My strength is in my ability to apply a unique balance of artistic and technical skills to web technologies and print in terms of design constraints and potential. And I'm always seeking out the latest, best-pactices to stay abreast of the market.
+                </p>
+                <p>
+                    Oh, and I love working with metal clay and Swarovski crystals. Metal clay is fine silver (also comes in other metals) mixed with a gluten to give it a clay consistency. The gluten is burned off in the kiln process and the final piece is fine silver. So as you've probably guessed, by my domain name, I'll be creating a site where I'll showcase and sell my jewelry. 
+                </p>
+            <h2>Name of code editor I use</h2>
+                <p>
+                    I'm transitioning from Dreamweaver to Sublime 2. I believe that all of this great local and server setup information will expedite that transition.
+                </p>
+        </div><!--end wrapper-->
+    </body>
 </html>
